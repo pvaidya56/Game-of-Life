6cb5f2e4e043ab46b60d983cd04fc0805c4f0254
Priya <pvaidya56@gmail.com>
2020-07-03 16:36:14 -0700

    game works, added rules

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -81,8 +81,40 @@ class Main extends React.Component {
     })
   }
 
+  playButton = () => {
+    clearInterval(this.intervalId)
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+    //rules
+    for (let i = 0; i < this.rows; i++) {
+		  for (let j = 0; j < this.cols; j++) {
+        //count is how many neighbors it has
+		    let count = 0;
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+		  }
+    }
+    this.setState({
+		  gridFull: g2,
+		  generation: this.state.generation + 1
+		});
+  }
+
   componentDidMount() {
     this.seed();
+    this.playButton();
   }
   render() {
     return (
